5612374bfae2bfd2e2f3a08b17a8bf874b7a4fb2
salik2010 <salomonsalik1993@mail.ru>
2023-04-02 23:55:47 +0700

    finish

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,7 +15,12 @@ public class Application {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
+
         }
+        Timer bean1 =
+                (Timer) applicationContext.getBean("timer");
+        System.out.println(bean1.getTime());
+
     }
 
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,13 @@
 package app.model;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("timer")
 public class Timer {
+
 
     private Long nanoTime = System.nanoTime();
 
